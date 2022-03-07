55662a662155e0f80c9231b96ce9c33ece3f0964
Yura Park <yrpr1014@gamil.com>
2022-03-07 13:19:03 +0900

    add helloworld

--- myfirstjava/src/main/java/myfirstjava/Hello.java
+++ myfirstjava/src/main/java/myfirstjava/Hello.java
@@ -3,7 +3,7 @@ package myfirstjava;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello world");
 		// branch1
 		// TODO Auto-generated method stub
 
